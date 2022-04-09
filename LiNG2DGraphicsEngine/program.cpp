e82a077e9684b4d5011a74aa923caec37f2445ea
FreakLign <32242776+FreakLign@users.noreply.github.com>
2022-04-09 17:54:14 +0800

    U

--- LiNG2DGraphicsEngine/program.cpp
+++ LiNG2DGraphicsEngine/program.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <Windows.h>
 #include <mmsystem.h>
 #include <mmreg.h>
@@ -5,11 +6,12 @@
 #include "BasicChart.h"
 #include <mutex>
 #include <ipps.h>
+#include <io.h>
 
 #pragma comment(lib, "winmm.lib")
 
 #define BUFSIZE				655360
-#define SAMPLERATE			6400
+#define SAMPLERATE			64000
 #define BITS_PER_SAMPLE		16
 #define CHANNEL_COUNT		1
 
@@ -25,17 +27,16 @@ int main() {
 	char* pBuffer1;//采集音频时的数据缓存  
 	WAVEHDR wHdr1; //采集音频时包含数据缓存的结构体
 	waveform.wFormatTag = WAVE_FORMAT_PCM;//声音格式为PCM  
-	waveform.nSamplesPerSec = SAMPLERATE;//采样率，16000次/秒  
+	waveform.nSamplesPerSec = SAMPLERATE;//采样率
 	waveform.wBitsPerSample = BITS_PER_SAMPLE;//采样比特，16bits/次  
-	waveform.nChannels = CHANNEL_COUNT;//采样声道数，2声道  
+	waveform.nChannels = CHANNEL_COUNT;//采样声道数，1声道  
 	waveform.nAvgBytesPerSec = SAMPLERATE * CHANNEL_COUNT * BITS_PER_SAMPLE / 8;//每秒的数据率，就是每秒能采集多少字节的数据  
 	waveform.nBlockAlign = CHANNEL_COUNT * BITS_PER_SAMPLE / 8;//一个块的大小，采样bit的字节数乘以声道数  
-	waveform.cbSize = 0;//通常为0 
+	waveform.cbSize = 0;
 	wait = CreateEvent(NULL, 0, 0, NULL);
-	//使用waveInOpen函数开启音频采集  
 	waveInOpen(&hWaveIn, WAVE_MAPPER, &waveform, (DWORD_PTR)wait, 0L, CALLBACK_EVENT);
-	//创建两个数组（这里能够创建多个数组）用来缓冲音频数据  
-	DWORD bufsize = BUFSIZE;//每次开辟10k的缓存存储录音数据  
+	DWORD bufsize = BUFSIZE;
+	FILE* f_towrite = fopen("dat0.dat", "wb");
 	thread t = thread([&] {
 		pBuffer1 = new char[SAMPLERATE * CHANNEL_COUNT * BITS_PER_SAMPLE / 80];
 		wHdr1.dwBufferLength = SAMPLERATE * CHANNEL_COUNT * BITS_PER_SAMPLE / 80;
@@ -54,13 +55,23 @@ int main() {
 		}
 		wHdr1.lpData = (LPSTR)pBuffer1 + pos_tail;
 		while (true) {
-			waveInPrepareHeader(hWaveIn, &wHdr1, sizeof(WAVEHDR));//准备一个波形数据块头用于录音  
-			waveInAddBuffer(hWaveIn, &wHdr1, sizeof(WAVEHDR));//指定波形数据块为录音输入缓存  
-			waveInStart(hWaveIn);//开始录音  
+			waveInPrepareHeader(hWaveIn, &wHdr1, sizeof(WAVEHDR));
+			waveInAddBuffer(hWaveIn, &wHdr1, sizeof(WAVEHDR));
+			waveInStart(hWaveIn);
 			Sleep(10);
 			ippsConvert_16s32f((short*)(pBuffer1), fTempData + BUFSIZE + (pos_tail / 2), wHdr1.dwBytesRecorded / 2);
-			//ippsLn_32f_I(fTempData + BUFSIZE + (pos_tail / 2), wHdr1.dwBytesRecorded / 2);
-			ippsDivC_32f_I(640.f, fTempData + BUFSIZE + (pos_tail / 2), wHdr1.dwBytesRecorded / 2);
+			for (auto fitem = fTempData + BUFSIZE + (pos_tail / 2); fitem < (fTempData + BUFSIZE + (pos_tail / 2) + wHdr1.dwBytesRecorded / 2); fitem++)
+			{
+				auto fv = (*fitem) / 1000.0;
+				if (fv > 0) {
+					(*fitem) = (1.0f - expf(0 - fv)) / 2.0f;
+				}
+				else {
+					(*fitem) = (expf(fv) - 1.0f) / 2.0f;
+				}
+			}
+			//fwrite(pBuffer1, wHdr1.dwBytesRecorded, 1, f_towrite);
+			//fwrite(fTempData + BUFSIZE + (pos_tail / 2), wHdr1.dwBytesRecorded * 2, 1, f_towrite);
 			ippsRealToCplx_32f(fIndex, fTempData + (pos_tail / 2), (Ipp32fc*)fPoints, BUFSIZE);
 			chart->InputData(fPoints, BUFSIZE);
 			pos_tail += wHdr1.dwBytesRecorded;
